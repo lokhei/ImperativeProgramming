fdae8f69c1d1e71d84aaeabf06a1028f688b3f32
lokhei <lokhei01@gmail.com>
2021-02-20 14:07:58 +0000

    update lab01

--- week01/limits.c
+++ week01/limits.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <limits.h>
+#include <float.h>
+
 
 int main(){
   printf("Largest int is %d\n", INT_MAX);
   printf("Smallest int is %d\n", INT_MIN);
-  /*printf("Largest character is %d\n", CHAR_MAX);
+  printf("Largest character is %d\n", CHAR_MAX);
   printf("Smallest character is %d\n", CHAR_MIN);
-  printf("Largest short is %d\n", SHORT_MAX);
-  printf("Smallest short is %d\n", SHORT_MIN);
+  printf("Largest short is %d\n", SHRT_MAX);
+  printf("Smallest short is %d\n", SHRT_MIN);
   printf("Largest long is %ld\n", LONG_MAX);
   printf("Smallest long is %ld\n", LONG_MIN);
-  printf("Largest float is %d\n", FLOAT_MAX);
-  printf("Smallest float is %d\n", FLOAT_MIN);
-  printf("Largest double is %d\n", DOUBLE_MAX);
-  printf("Samllest double is %d\n", DOUBLE_MIN);*/
+  printf("Largest float is %f\n", FLT_MAX);
+  printf("Smallest float is %f\n", FLT_MIN);
+  printf("Largest double is %f\n", DBL_MAX);
+  printf("Smallest double is %f\n", DBL_MIN);
   return 0;
 }
